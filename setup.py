e2755b0e732ef3c764745e0428ba000a154fe7f9
razi1997 <62413549+razi1997@users.noreply.github.com>
2023-08-17 02:17:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 DESCRIPTION = 'Facebook Video Downloading via Python Package'
 LONG_DESCRIPTION = 'A package that allows you to download video from Facebook'
 
